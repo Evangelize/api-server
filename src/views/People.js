85907d087299fd7cee7bcfe2751da842edbc660a
Matthew Voss <mattv@tamu.edu>
2016-01-13 16:23:33 -0600

    update people view

--- src/views/People.js
+++ src/views/People.js
@@ -21,6 +21,7 @@ import ToolbarSeparator from 'material-ui/lib/toolbar/toolbar-separator';
 import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title';
 import RaisedButton from 'material-ui/lib/raised-button';
 import Toggle from 'material-ui/lib/toggle';
+import { Grid, Row, Col } from 'react-bootstrap';
 import { getPeople, setPerson } from '../actions';
 
 class People extends Component {
@@ -93,79 +94,95 @@ class People extends Component {
     const { dispatch, people, ...props } = this.props;
            console.log(people);
     return (
-      <div className={"mdl-grid"}>
-        <div className={"mdl-cell mdl-cell--12-col"}>
-          <div className={"mdl-typography--title-color-contrast"} style={{opacity: ".57"}}>People</div>
-        </div>
-        <div className={"mdl-cell mdl-cell--12-col"}>
-          <DashboardMedium title={"Find Members"} subtitle={"Search for members to provision as students and teachers"}>
-            <div style={divStyle}>
-              <DropDownMenu
-                value={searchType}
-                onChange={::this.handleSelectValueChange}
-                style={dropDownStyle}>
-                <MenuItem value={"lastName"} primaryText="Last Name" />
-                <MenuItem value={"firstName"} primaryText="First Name" />
-                <MenuItem value={"emailAddress"} primaryText="Email" />
-              </DropDownMenu>
-              <TextField
-                ref="searchField"
-                floatingLabelText="Search"
-                defaultValue={people.filter}
-                onChange={::this._handleInputChange} />
-            </div>
-            <div>
-              <Table
-                height={this.state.height}
-                fixedHeader={this.state.fixedHeader}
-                fixedFooter={this.state.fixedFooter}
-                selectable={this.state.selectable}
-                multiSelectable={this.state.multiSelectable}
-                onRowSelection={this._onRowSelection}>
-                <TableHeader
-                  adjustForCheckbox={this.state.adjustForCheckbox}
-                  displaySelectAll={this.state.displaySelectAll}
-                  enableSelectAll={this.state.enableSelectAll}>
-                  <TableRow>
-                    <TableHeaderColumn style={{ width: "50%" }}>Name</TableHeaderColumn>
-                    <TableHeaderColumn style={{ width: "25%" }}>Teacher</TableHeaderColumn>
-                    <TableHeaderColumn style={{ width: "25%" }}>Student</TableHeaderColumn>
-                  </TableRow>
-                </TableHeader>
-                <TableBody
-                  displayRowCheckbox={this.state.displayRowCheckbox}
-                  deselectOnClickaway={this.state.deselectOnClickaway}
-                  showRowHover={this.state.showRowHover}
-                  stripedRows={this.state.stripedRows}>
-                  {people.data.map((person, index) =>
-                    <TableRow selected={false} key={person.id}>
-                      <TableRowColumn style={{ width: "50%" }}>
-                        <div>{person.lastName}, {person.firstName}</div>
-                        <div className={"mdl-typography--caption-color-contrast"}>{person.familyName}</div>
-                      </TableRowColumn>
-                      <TableRowColumn style={{ width: "25%" }}>
-                        <Toggle
-                          name="toggleTeacher"
-                          value="teacher"
-                          defaultToggled={person.teacher}
-                          onToggle={(e, toggle) => ::this._handlePersonToggle(e, toggle, index)}  />
-                      </TableRowColumn>
-                      <TableRowColumn style={{ width: "25%" }}>
-                        <Toggle
-                          name="toggleStudent"
-                          value="student"
-                          defaultToggled={person.student}
-                          onToggle={(e, toggle) => ::this._handlePersonToggle(e, toggle, index)} />
-                      </TableRowColumn>
+      <Grid fluid={true}>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={12}>
+            <nav className={"grey darken-1"}>
+              <div className={"nav-wrapper"}>
+                <div className={"col s12 m12 l12"}>
+                  <a href="#!" className={"breadcrumb"}>Dashboard</a>
+                  <a href="#!" className={"breadcrumb"}>People</a>
+                </div>
+              </div>
+            </nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={12}>
+            <DashboardMedium title={"Find Members"} subtitle={"Search for members to provision as students and teachers"}>
+              <Row>
+                <Col xs={12} sm={12} md={3} lg={2}>
+                  <DropDownMenu
+                    value={searchType}
+                    onChange={::this.handleSelectValueChange}
+                    style={dropDownStyle}>
+                    <MenuItem value={"lastName"} primaryText="Last Name" />
+                    <MenuItem value={"firstName"} primaryText="First Name" />
+                    <MenuItem value={"emailAddress"} primaryText="Email" />
+                  </DropDownMenu>
+                </Col>
+                <Col xs={12} sm={12} md={9} lg={10}>
+                  <TextField
+                    className={"searchBox"}
+                    ref="searchField"
+                    floatingLabelText="Search"
+                    defaultValue={people.filter}
+                    onChange={::this._handleInputChange} />
+                </Col>
+              </Row>
+              <div>
+                <Table
+                  height={this.state.height}
+                  fixedHeader={this.state.fixedHeader}
+                  fixedFooter={this.state.fixedFooter}
+                  selectable={this.state.selectable}
+                  multiSelectable={this.state.multiSelectable}
+                  onRowSelection={this._onRowSelection}>
+                  <TableHeader
+                    adjustForCheckbox={this.state.adjustForCheckbox}
+                    displaySelectAll={this.state.displaySelectAll}
+                    enableSelectAll={this.state.enableSelectAll}>
+                    <TableRow>
+                      <TableHeaderColumn style={{ width: "50%" }}>Name</TableHeaderColumn>
+                      <TableHeaderColumn style={{ width: "25%" }}>Teacher</TableHeaderColumn>
+                      <TableHeaderColumn style={{ width: "25%" }}>Student</TableHeaderColumn>
                     </TableRow>
-                  )}
+                  </TableHeader>
+                  <TableBody
+                    displayRowCheckbox={this.state.displayRowCheckbox}
+                    deselectOnClickaway={this.state.deselectOnClickaway}
+                    showRowHover={this.state.showRowHover}
+                    stripedRows={this.state.stripedRows}>
+                    {people.data.map((person, index) =>
+                      <TableRow selected={false} key={person.id}>
+                        <TableRowColumn style={{ width: "50%" }}>
+                          <div>{person.lastName}, {person.firstName}</div>
+                          <div className={"mdl-typography--caption-color-contrast"}>{person.familyName}</div>
+                        </TableRowColumn>
+                        <TableRowColumn style={{ width: "25%" }}>
+                          <Toggle
+                            name="toggleTeacher"
+                            value="teacher"
+                            defaultToggled={person.teacher}
+                            onToggle={(e, toggle) => ::this._handlePersonToggle(e, toggle, index)}  />
+                        </TableRowColumn>
+                        <TableRowColumn style={{ width: "25%" }}>
+                          <Toggle
+                            name="toggleStudent"
+                            value="student"
+                            defaultToggled={person.student}
+                            onToggle={(e, toggle) => ::this._handlePersonToggle(e, toggle, index)} />
+                        </TableRowColumn>
+                      </TableRow>
+                    )}
 
-                </TableBody>
-              </Table>
-            </div>
-          </DashboardMedium>
-        </div>
-      </div>
+                  </TableBody>
+                </Table>
+              </div>
+            </DashboardMedium>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
