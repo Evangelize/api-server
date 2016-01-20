2ab6721a088ea7ab943e67680e4e87de9f575747
Matthew Voss <mattv@tamu.edu>
2016-01-20 14:25:12 -0600

    update models getters to format date as unixms

--- src/models/DivisionClassTeachers.js
+++ src/models/DivisionClassTeachers.js
@@ -7,9 +7,22 @@ module.exports = function (sequelize, DataTypes) {
       "peopleId": DataTypes.INTEGER,
       "day": DataTypes.INTEGER,
       "confirmed": DataTypes.BOOLEAN,
-      "createdAt": DataTypes.DATE,
+      "createdAt": {
+        type: DataTypes.DATE,
+        get: function()  {
+          if (this.getDataValue('createdAt')) {
+            return this.getDataValue('createdAt').getTime();
+          } else {
+            return null;
+          }
+        }
+      },
       "updatedAt": DataTypes.DATE,
-      "deletedAt": DataTypes.DATE
+      "deletedAt": DataTypes.DATE,
+      "revision": {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      }
     },
     {
       paranoid: true,
