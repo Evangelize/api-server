9d9889587141ba508a3ec6b4204a91c5fd27c531
Matthew Voss <voss.matthew@gmail.com>
2017-03-18 23:32:53 -0500

    2nd temp fix

--- src/lib/neat-csv.js
+++ src/lib/neat-csv.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 'use strict';
-const intoStream = require('into-stream');
+//const intoStream = require('into-stream');
 const csvParser = require('csv-parser');
 //const getStream = require('get-stream');
 
 module.exports = (input, opts) => {
   if (typeof input === 'string' || Buffer.isBuffer(input)) {
-    input = intoStream(input);
+    //input = intoStream(input);
   }
 
   //return getStream.array(input.pipe(csvParser(opts)));
